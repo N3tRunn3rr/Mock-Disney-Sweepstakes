8f5cd5059e38057eb74fc8db520c2f3b2795f3c6
boy-son <59755443+boy-son@users.noreply.github.com>
2023-10-16 18:45:14 -0400

    fixed slideshow, aligned logos at top

--- Front-end/JS/home.js
+++ Front-end/JS/home.js
@@ -1,16 +1,28 @@
 var slideIndex = 0;
-    showSlides();
+showSlides();
 
-    function showSlides() {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        slideIndex++;
-        if (slideIndex > slides.length) {
-            slideIndex = 1;
-        }
-        slides[slideIndex - 1].style.display = "block";
-        setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    
+    if (slides.length === 0) {
+      return;
+    }
+  
+    if (slideIndex < 1) {
+      slideIndex = slides.length;
+    }
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    
+    slideIndex++;
+    
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
