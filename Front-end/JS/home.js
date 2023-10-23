eb3ede45b4bd35ee9f2fd178885ff90786da2caa
boy-son <59755443+boy-son@users.noreply.github.com>
2023-10-23 14:12:28 -0400

    added fade in and fade out to slideshow

--- Front-end/JS/home.js
+++ Front-end/JS/home.js
@@ -1,28 +1,66 @@
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+    
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+
+//     slideIndex++;
+
+//     if (slideIndex > slides.length) {
+//         slideIndex = 1;
+//     }
+
+//     slides[slideIndex - 1].style.display = "block";
+
+//     setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+
 var slideIndex = 0;
 showSlides();
 
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    
-    if (slides.length === 0) {
-      return;
-    }
-  
-    if (slideIndex < 1) {
-      slideIndex = slides.length;
-    }
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
-    }
-    
+
     for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+        slides[i].style.display = "none";
+        slides[i].style.opacity = 0; 
     }
 
-    slides[slideIndex - 1].style.display = "block";
-    
     slideIndex++;
-    
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-  }
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    var currentSlide = slides[slideIndex - 1];
+    currentSlide.style.display = "block";
+
+    var opacity = 0;
+    var fadeInInterval = setInterval(function () {
+        if (opacity >= 1) {
+            clearInterval(fadeInInterval);
+            setTimeout(fadeOut, 1800); 
+        } else {
+            opacity += 0.02; 
+            currentSlide.style.opacity = opacity;
+        }
+    }, 20); 
+
+    function fadeOut() {
+        var fadeOutInterval = setInterval(function () {
+            if (opacity <= 0) {
+                clearInterval(fadeOutInterval);
+                showSlides();
+            } else {
+                opacity -= 0.02;
+                currentSlide.style.opacity = opacity;
+            }
+        }, 20); 
+    }
+}
